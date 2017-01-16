038ad89d3612b66dddad9919ac4092bce2808aec
tony <tonyofbyteball@users.noreply.github.com>
2017-01-16 12:17:41 +0300

    testnet

--- bitcoin_client.js
+++ bitcoin_client.js
@@ -1,10 +1,13 @@
 /*jslint node: true */
 'use strict';
 var bitcoin = require('bitcoin');
+var constants = require('byteballcore/constants.js');
+
+var bTestnet = constants.version.match(/t$/);
 
 var client = new bitcoin.Client({
 	host: 'localhost',
-	port: 18332,
+	port: bTestnet ? 18332 : 8332,
 	user: 'bitcoin',
 	pass: 'local321',
 	timeout: 30000
