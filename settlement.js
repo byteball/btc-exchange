1d84c3af05af04788c3f2dc8101f2213416507bc
tony <tonyofbyteball@users.noreply.github.com>
2017-02-04 03:34:08 +0300

    to string

--- settlement.js
+++ settlement.js
@@ -16,7 +16,7 @@ var notifications = require('./notifications.js');
 function sendBtc(amount, address, onDone){
 	client.sendToAddress(address, amount, function(err, txid, resHeaders) {
 		console.log('bitcoin sendToAddress '+address+', amount '+amount+', txid '+txid+', err '+err);
-		if (err === 'Error: Transaction amount too small'){
+		if (err && err+'' === 'Error: Transaction amount too small'){
 			err = null;
 			txid = 'too small';
 		}
